517a0a2252cc32afb5547a8687fed4041c3b7b21
Aaron Samuel <aasamuelj@gmail.com>
2017-09-22 09:30:02 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,7 @@
 # code your #valid_move? method here
 require 'pry'
 def valid_move?(board, index)
-  binding.pry
-  index.to_i.between?(1,9) && !position_taken?(board, index)
+  index.to_i.between?(0,8) && !position_taken?(board, index)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
